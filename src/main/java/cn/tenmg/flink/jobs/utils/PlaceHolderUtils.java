34b7ba18a27b8f1d3d2e64ea0bace572fece5797
zhaoweijun <wjzhao@aliyun.com>
2022-01-07 09:52:05 +0800

    修复因变量中含有“$”或“\”时占位符替换报错或不正确的问题

--- src/main/java/cn/tenmg/flink/jobs/utils/PlaceHolderUtils.java
+++ src/main/java/cn/tenmg/flink/jobs/utils/PlaceHolderUtils.java
@@ -1,12 +1,12 @@
 package cn.tenmg.flink.jobs.utils;
 
 import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import cn.tenmg.dsl.utils.DSLUtils;
 import cn.tenmg.dsl.utils.ParamsUtils;
 import cn.tenmg.dsl.utils.StringUtils;
 
@@ -19,8 +19,19 @@ import cn.tenmg.dsl.utils.StringUtils;
  */
 public abstract class PlaceHolderUtils {
 
-	private static final Pattern paramPattern = Pattern.compile("\\$\\{[^}]+\\}"),
-			arrayPattern = Pattern.compile("\\[[^\\]]+\\]");
+	private static final Pattern paramPattern = Pattern.compile("\\$\\{[^}]+\\}");
+
+	private static final HashSet<Character> ENCODE_CHARACTERS = new HashSet<Character>() {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -1974796098018071303L;
+
+		{
+			add('$');
+		}
+	};
 
 	/**
 	 * 将模板字符串中占位符替换为指定的参数
@@ -60,123 +71,57 @@ public abstract class PlaceHolderUtils {
 		} else {
 			StringBuffer sb = new StringBuffer();
 			Matcher m = paramPattern.matcher(tpl);
-			String name;
+			String name, s;
 			Object value;
+			boolean encode = false;
 			while (m.find()) {
 				name = m.group();
 				value = ParamsUtils.getParam(params, name.substring(2, name.length() - 1));
 				if (value == null) {
 					m.appendReplacement(sb, "");
 				} else {
-					m.appendReplacement(sb, value.toString());
+					s = value.toString();
+					char c;
+					StringBuilder encoded = new StringBuilder();
+					for (int i = 0, len = s.length(); i < len; i++) {
+						c = s.charAt(i);
+						if (c == DSLUtils.BACKSLASH) {
+							encode = true;
+							encoded.append(DSLUtils.BACKSLASH).append(DSLUtils.BACKSLASH).append(DSLUtils.BACKSLASH)
+									.append(DSLUtils.BACKSLASH);
+						} else {
+							if (ENCODE_CHARACTERS.contains(c)) {
+								encode = true;
+								encoded.append(DSLUtils.BACKSLASH);
+							}
+							encoded.append(c);
+						}
+					}
+					m.appendReplacement(sb, encoded.toString());
 				}
+			}
+			if (encode) {
+				StringBuffer uncode = new StringBuffer();
+				int i = 0, len = sb.length();
+				char c, n;
+				while (i < len) {
+					c = sb.charAt(i);
+					if (++i < len && c == DSLUtils.BACKSLASH) {
+						n = sb.charAt(i);
+						if (n == DSLUtils.BACKSLASH) {
+							i++;
+							uncode.append(c);
+						} else if (!ENCODE_CHARACTERS.contains(n)) {
+							uncode.append(c);
+						}
+					} else {
+						uncode.append(c);
+					}
+				}
+				sb = uncode;
 			}
 			m.appendTail(sb);
 			return sb.toString();
-		}
-	}
-
-	/**
-	 * 获取参数集中的参数值。参数名支持使用“key.name”访问参数值的属性值，层级数不限，支持使用“[*]”访问数组值，维数不限，“key.name”和“[*]”也可以配合使用。已废弃，将在下一版本移除，请使用cn.tenmg.dsl.utils.ParamsUtils.getParam替换
-	 * 
-	 * @param params
-	 *            参数集
-	 * @param name
-	 *            参数名
-	 * @return 如果参数集的参数存在则，返回它的值；否则，返回null
-	 */
-	@Deprecated
-	public static Object getParam(Map<?, ?> params, String name) {
-		Object value = params.get(name);
-		if (value == null) {
-			if (name.contains(".")) {// 访问Bean或者Map属性
-				String[] names = name.split("\\.");
-				name = names[0];
-				value = params.get(name);
-				if (value == null) {// 如果类似“key.name[*]……”形式的，可能是访问数组的某一项值或者是访问Map对象的属性值。如果是，则获取数组的某一项值或者Map对象的某个属性值
-					return getMaybeArrayOrMapValue(params, name);
-				} else {
-					for (int i = 1; i < names.length; i++) {
-						name = names[i];
-						value = ObjectUtils.getValue(value, name);// 获取对象属性
-						if (value == null) {// 可能是数组
-							Matcher m = arrayPattern.matcher(name);
-							if (m.find()) {// 含有数组访问符“[]”
-								value = ObjectUtils.getValue(value, name.substring(0, name.indexOf("[")));// 获取数组对象
-								if (value == null) {// 数组对象为null，返回null
-									return null;
-								} else {// 否则，获取数组的值
-									value = getArrayOrMapValue(value, params, m);
-								}
-							}
-							return value;
-						}
-					}
-					return value;
-				}
-			} else {// 如果类似“key[*]……”形式的，可能是访问数组的某一项值或者是访问Map对象的属性值。如果是，则获取数组的某一项值或者Map对象的某个属性值
-				return getMaybeArrayOrMapValue(params, name);
-			}
-		} else {
-			return value;
-		}
-	}
-
-	/**
-	 * 获取可能通过“[*]”方式访问的数组某一项值或Map对象属性值
-	 * 
-	 * @param params
-	 *            参数集
-	 * @param name
-	 *            参数名
-	 * @return 如果含有“[*]”符号，则获取数组的值或者Map对象的属性值；否则返回null
-	 */
-	@Deprecated
-	private static final Object getMaybeArrayOrMapValue(Map<?, ?> params, String name) {
-		Object value = null;
-		Matcher m = arrayPattern.matcher(name);
-		if (m.find()) {
-			value = params.get(name.substring(0, name.indexOf("[")));
-			if (value == null) {
-				return null;
-			} else {// 继续获取下一维数组的值
-				value = getArrayOrMapValue(value, params, m);
-			}
-		}
-		return value;
-	}
-
-	@Deprecated
-	private static final Object getArrayOrMapValue(Object value, Map<?, ?> params, Matcher m) {
-		value = getArrayOrMapValue(value, params, m.group());
-		while (value != null && m.find()) {
-			value = getArrayOrMapValue(value, params, m.group());
-		}
-		return value;
-	}
-
-	@Deprecated
-	private static final Object getArrayOrMapValue(Object value, Map<?, ?> params, String group) {
-		String name = group.substring(1, group.length() - 1);
-		return getValue(value, params, name);
-	}
-
-	private static final Object getValue(Object value, Map<?, ?> params, String name) {
-		Object v = params.get(name);
-		String real = name;
-		if (v != null) {
-			real = v.toString();
-		}
-		if (value instanceof Map) {
-			return ((Map<?, ?>) value).get(real);
-		} else if (value instanceof List) {
-			return ((List<?>) value).get(Integer.valueOf(real));
-		} else if (value instanceof Object[]) {
-			return ((Object[]) value)[Integer.valueOf(real)];
-		} else if (value instanceof LinkedHashSet) {
-			return ((LinkedHashSet<?>) value).toArray()[Integer.valueOf(real)];
-		} else {
-			return null;
 		}
 	}
 }
