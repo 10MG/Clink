ff033ecace8774c79c59c8703ed748116fcd98c3
zhaoweijun <wjzhao@aliyun.com>
2022-02-10 10:46:45 +0800

    完善测试用例

--- flink-jobs-clients/src/test/java/cn/tenmg/flink/jobs/clients/StandaloneRestClusterClientTest.java
+++ flink-jobs-clients/src/test/java/cn/tenmg/flink/jobs/clients/StandaloneRestClusterClientTest.java
@@ -1,6 +1,7 @@
 package cn.tenmg.flink.jobs.clients;
 
 import org.apache.flink.api.common.JobID;
+import org.apache.flink.api.common.JobStatus;
 
 import cn.tenmg.flink.jobs.clients.utils.ClassUtils;
 import cn.tenmg.flink.jobs.config.loader.XMLConfigLoader;
@@ -23,7 +24,12 @@ public class StandaloneRestClusterClientTest {
 		JobID jobId = client.submit(flinkJobs);
 		System.out.println("Flink job launched: " + jobId.toHexString());// 启动flink-jobs作业
 		Thread.sleep(80000);
-		System.out
-				.println("Flink job of jobId: " + jobId.toHexString() + " stopped, savepoint path: " + client.stop(jobId));// 停止flink-jobs作业
+
+		// JobID jobId = JobID.fromHexString(hexString);
+		JobStatus jobStatus = client.getJobStatus(jobId);// 获取作业状态
+		System.out.println("Job status: " + jobStatus);
+
+		System.out.println(
+				"Flink job of jobId: " + jobId.toHexString() + " stopped, savepoint path: " + client.stop(jobId));// 停止flink-jobs作业
 	}
 }
