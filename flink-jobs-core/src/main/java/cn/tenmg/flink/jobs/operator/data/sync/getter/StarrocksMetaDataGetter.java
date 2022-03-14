5eb4f39c1e9465dc1f81010b7f86de21a91b9e62
zhaoweijun <wjzhao@aliyun.com>
2022-03-14 15:02:46 +0800

    支持自动获取StarRocks的主键信息

--- flink-jobs-core/src/main/java/cn/tenmg/flink/jobs/operator/data/sync/getter/StarrocksMetaDataGetter.java
+++ flink-jobs-core/src/main/java/cn/tenmg/flink/jobs/operator/data/sync/getter/StarrocksMetaDataGetter.java
@@ -39,11 +39,11 @@ public class StarrocksMetaDataGetter extends AbstractJDBCMetaDataGetter {
 	@Override
 	protected Set<String> getPrimaryKeys(Connection con, String catalog, String schema, String tableName)
 			throws SQLException {
-		StringBuilder sqlBuilder = new StringBuilder("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE");
+		StringBuilder sqlBuilder = new StringBuilder("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_KEY = 'PRI'");
 		if (schema != null) {
-			sqlBuilder.append(" TABLE_SCHEMA = ? AND");
+			sqlBuilder.append(" AND TABLE_SCHEMA = ?");
 		}
-		sqlBuilder.append(" TABLE_NAME = ? ORDER BY ORDINAL_POSITION");
+		sqlBuilder.append(" AND TABLE_NAME = ? ORDER BY ORDINAL_POSITION");
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
