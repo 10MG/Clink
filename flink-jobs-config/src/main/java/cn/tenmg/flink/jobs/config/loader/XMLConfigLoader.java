63cc72e79d8efad81d9e8920b002e24725f7a22a
June <June@DESKTOP-CM2PBAJ>
2022-12-06 10:58:43 +0800

    修复在并行解析配置时，由于重用Unmarshaller带来的线程安全问题

--- flink-jobs-config/src/main/java/cn/tenmg/flink/jobs/config/loader/XMLConfigLoader.java
+++ flink-jobs-config/src/main/java/cn/tenmg/flink/jobs/config/loader/XMLConfigLoader.java
@@ -23,15 +23,7 @@ public class XMLConfigLoader implements ConfigLoader {
 
 	private static final XMLConfigLoader INSTANCE = new XMLConfigLoader();
 
-	private static Unmarshaller unmarshaller;
-
-	static {
-		try {
-			unmarshaller = JAXBContext.newInstance(FlinkJobs.class).createUnmarshaller();
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
-	}
+	private static JAXBContext context;
 
 	private XMLConfigLoader() {
 		super();
@@ -44,13 +36,12 @@ public class XMLConfigLoader implements ConfigLoader {
 	/**
 	 * 加载flink-jobs配置
 	 * 
-	 * @param s
-	 *            配置字符串
+	 * @param s 配置字符串
 	 * @return flink-jobs配置模型
 	 */
 	public FlinkJobs load(String s) {
 		try {
-			return (FlinkJobs) unmarshaller.unmarshal(new StringReader(s));
+			return (FlinkJobs) newUnmarshaller().unmarshal(new StringReader(s));
 		} catch (JAXBException e) {
 			throw new IllegalArgumentException("Failed to load the flink-jobs configuration", e);
 		}
@@ -59,13 +50,12 @@ public class XMLConfigLoader implements ConfigLoader {
 	/**
 	 * 加载flink-jobs配置
 	 * 
-	 * @param file
-	 *            配置文件
+	 * @param file 配置文件
 	 * @return flink-jobs配置模型
 	 */
 	public FlinkJobs load(File file) {
 		try {
-			return (FlinkJobs) unmarshaller.unmarshal(file);
+			return (FlinkJobs) newUnmarshaller().unmarshal(file);
 		} catch (JAXBException e) {
 			throw new IllegalArgumentException("Failed to load the flink-jobs configuration", e);
 		}
@@ -74,13 +64,12 @@ public class XMLConfigLoader implements ConfigLoader {
 	/**
 	 * 加载flink-jobs配置
 	 * 
-	 * @param fr
-	 *            文件读取器
+	 * @param fr 文件读取器
 	 * @return flink-jobs配置模型
 	 */
 	public FlinkJobs load(FileReader fr) {
 		try {
-			return (FlinkJobs) unmarshaller.unmarshal(fr);
+			return (FlinkJobs) newUnmarshaller().unmarshal(fr);
 		} catch (JAXBException e) {
 			throw new IllegalArgumentException("Failed to load the flink-jobs configuration", e);
 		}
@@ -89,16 +78,26 @@ public class XMLConfigLoader implements ConfigLoader {
 	/**
 	 * 加载flink-jobs配置
 	 * 
-	 * @param is
-	 *            输入流
+	 * @param is 输入流
 	 * @return flink-jobs配置模型
 	 */
 	@Override
 	public FlinkJobs load(InputStream is) {
 		try {
-			return (FlinkJobs) unmarshaller.unmarshal(is);
+			return (FlinkJobs) newUnmarshaller().unmarshal(is);
 		} catch (JAXBException e) {
 			throw new IllegalArgumentException("Failed to load the flink-jobs configuration", e);
 		}
+	}
+
+	private Unmarshaller newUnmarshaller() throws JAXBException {
+		if (context == null) {
+			synchronized (context) {
+				if (context == null) {
+					context = JAXBContext.newInstance(FlinkJobs.class);
+				}
+			}
+		}
+		return context.createUnmarshaller();
 	}
 }
